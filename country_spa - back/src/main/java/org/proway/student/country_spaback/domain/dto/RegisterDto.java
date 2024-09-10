c01a51c1bcd46b3b78a81f35327fa2cd3f634e93
kingabreel <gabrielyury441@gmail.com>
2024-09-10 15:19:07 -0300

    refactor: register input

--- country_spa - back/src/main/java/org/proway/student/country_spaback/domain/dto/RegisterDto.java
+++ country_spa - back/src/main/java/org/proway/student/country_spaback/domain/dto/RegisterDto.java
@@ -1,5 +1,3 @@
 package org.proway.student.country_spaback.domain.dto;
 
-import org.proway.student.country_spaback.domain.Role;
-
-public record RegisterDto(String name, String email, String password, Role role){}
+public record RegisterDto(String name, String email, String password){}
